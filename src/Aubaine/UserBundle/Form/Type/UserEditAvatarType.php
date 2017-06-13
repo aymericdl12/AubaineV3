dee24bf1f32c4257b496137fa6d5b6c2055df88a
Aymeric de Lanouvelle <aymeric.lanouvelle@gmail.com>
2017-06-13 15:21:15 +0200

    changement upload avatar

---
+++ src/Aubaine/UserBundle/Form/Type/UserEditAvatarType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Aubaine\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+class UserEditAvatarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add( 'imageFile', FileType::class )
+        ->add('save',      SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Aubaine\UserBundle\Document\User',
+        ));
+    }
+}
---
+++ src/Aubaine/UserBundle/Form/Type/UserEditPasswordType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aubaine\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserEditPasswordType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+    ->add('password',     TextType::class)
+    ->add('save',      SubmitType::class);
+    ;
+    
+  }
+}
