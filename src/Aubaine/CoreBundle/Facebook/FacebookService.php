de0008fbc98ff0273064e1af4aac6089f847799d
Aymeric de Lanouvelle <aymeric.lanouvelle@gmail.com>
2017-09-01 11:23:22 +0200

    0908

--- src/Aubaine/CoreBundle/Facebook/FacebookService.php
+++ src/Aubaine/CoreBundle/Facebook/FacebookService.php
@@ -41,7 +41,7 @@ class FacebookService
 		try {
 		  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
 		  // If you provided a 'default_access_token', the '{access-token}' is optional.
-		  $response = $this->connection->get('/me','{EAACEdEose0cBAPmz5TcZByX7KZAI8ZBlM1s2OZCZCtpdddrDRTAZAYlBQnPkxxk4gtOZAbTa5GHxxzIVLyveRQEA3OZBvcT8SCPOEif0ZBDLUw5kEvx0NkRZBvFQiPtpuKbXO4dDX7lePuPQJT5qOZCZC3J67fgN0FDYZBUTzhrk401sZCijMU3Gvm4lUCuuEbXTLVIwwZD}');
+		  $response = $this->connection->get('/me','EAACEdEose0cBAPmz5TcZByX7KZAI8ZBlM1s2OZCZCtpdddrDRTAZAYlBQnPkxxk4gtOZAbTa5GHxxzIVLyveRQEA3OZBvcT8SCPOEif0ZBDLUw5kEvx0NkRZBvFQiPtpuKbXO4dDX7lePuPQJT5qOZCZC3J67fgN0FDYZBUTzhrk401sZCijMU3Gvm4lUCuuEbXTLVIwwZD');
 		} catch(\Facebook\Exceptions\FacebookResponseException $e) {
 		  // When Graph returns an error
 		  return 'Graph returned an error: ' . $e->getMessage();
