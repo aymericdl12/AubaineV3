190ec11bdae66278b139aac9c5fca0383f4f6a5a
Aymeric de Lanouvelle <aymeric.lanouvelle@gmail.com>
2017-09-01 10:30:41 +0200

    0908

--- src/Aubaine/CoreBundle/Facebook/FacebookService.php
+++ src/Aubaine/CoreBundle/Facebook/FacebookService.php
@@ -22,8 +22,8 @@ class FacebookService
 
         // Cette instruction nous permettra de nous connecter à l'API
 		$this->connection = new \Facebook\Facebook([
-		  'app_id' => $this->appId,
-		  'app_secret' => $this->appSecret,
+		  'app_id' => 639260759607903,
+		  'app_secret' => '6e1639828958455d8a6549ff4f275190',
 		  'default_graph_version' => 'v2.10',
 		  'default_access_token' => $this->token,
 		]);
