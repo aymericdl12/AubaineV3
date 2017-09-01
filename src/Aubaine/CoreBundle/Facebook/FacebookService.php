4b73f5523d18225ad2576dbdc25d58fab5ab32fd
Aymeric de Lanouvelle <aymeric.lanouvelle@gmail.com>
2017-09-01 10:54:56 +0200

    0908

--- src/Aubaine/CoreBundle/Facebook/FacebookService.php
+++ src/Aubaine/CoreBundle/Facebook/FacebookService.php
@@ -23,12 +23,12 @@ class FacebookService
 		$this->appsecret_proof= hash_hmac('sha256', $token, $appSecret); 
 
         // Cette instruction nous permettra de nous connecter à l'API
-		$this->connection = new \Facebook\Facebook([
+		$this->connection = new Facebook\Facebook([
 		  'app_id' => 639260759607903,
 		  'app_secret' => '6e1639828958455d8a6549ff4f275190',
-		  'appsecret_proof' => $this->appsecret_proof,
-		  'default_graph_version' => 'v2.10',
-		  'default_access_token' => $this->token,
+		  // 'appsecret_proof' => $this->appsecret_proof,
+		  'default_graph_version' => 'v2.10'
+		  // 'default_access_token' => $this->token,
 		]);
 
 	}
@@ -40,7 +40,7 @@ class FacebookService
 		try {
 		  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
 		  // If you provided a 'default_access_token', the '{access-token}' is optional.
-		  $response = $this->connection->get('/me');
+		  $response = $this->connection->get('/me','EAACEdEose0cBAB2DNGxJh608bYIUmREkK7ZCUZCKB31SZBrp39TZBGTOmFMfIPqyg1bYZCLv68KbpitkJPKHPdvNva564LjhSqqcjBiRiHPOOtibIxu2r8Oa06mK42aJ7gbZAg3ILnZC62HgOoye6Og1FxkedQ5kXPKZAHnr1u5qZCktEhidCA9y08X51qW9mqcstOPQIVnhkBgZDZD');
 		} catch(\Facebook\Exceptions\FacebookResponseException $e) {
 		  // When Graph returns an error
 		  return 'Graph returned an error: ' . $e->getMessage();
