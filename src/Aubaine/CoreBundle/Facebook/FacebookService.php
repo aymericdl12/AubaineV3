691c6689ad20d7fa14ae43514f277295316e8248
Aymeric de Lanouvelle <aymeric.lanouvelle@gmail.com>
2017-09-01 11:50:09 +0200

    0908

--- src/Aubaine/CoreBundle/Facebook/FacebookService.php
+++ src/Aubaine/CoreBundle/Facebook/FacebookService.php
@@ -28,7 +28,7 @@ class FacebookService
 		  'app_id' => '639260759607903',
 		  'app_secret' => '6e1639828958455d8a6549ff4f275190',
 		  'appsecret_proof' => hash_hmac('sha256', '639260759607903', '6e1639828958455d8a6549ff4f275190'),
-		  'default_graph_version' => 'v2.10'
+		  'default_graph_version' => 'v2.10',
 		  'default_access_token' => 'EAAJFZA35ZByl8BAMxibAPsM7z6ZClEtVZBjHXzoL7MS5GZBv6qBbZAIwtLLTluQpqFYKK3ekgcpzZAZCcNh28bPZCbr0Mwdb3ncK53hbYyiZBoflnZAaF6PxfGAwvo2k5e7GpniqFZCvgmNbmza2TLW9q9LlKkEVa3wsE01ZCkGhJBZAQj5Q0EqxIBZAIdMaWZB2yzQ4EjUZD',
 		]);
 
